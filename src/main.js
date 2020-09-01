bddca7f578e7da0cd385b7e26ccb846683b83742
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-09-01 15:16:28 -0400

    remove conditional for attaching app

--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@ import App from './App.svelte';
 import Brief from './BriefOnly.svelte';
 
 
-if (document.getElementById('covid-testing-dashboard')) {
+
 	const app = new App({
 		target: document.getElementById('covid-testing-dashboard')
 	});
 	export default app;
-}
-
-
-
-// const app = new Brief({
-// 	target: document.getElementById('covid-test-brief')
-// });
