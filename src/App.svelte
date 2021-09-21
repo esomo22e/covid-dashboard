df8acc0b0742936723f5fbbc9e2d7c9e878a6d91
esomo22e <eesomonu@gmail.com>
2021-09-21 04:40:10 -0400

    updated for 9/18

--- src/App.svelte
+++ src/App.svelte
@@ -862,7 +862,7 @@
             <!-- Dashboard Donut Chart For the week and Get Tested (so stats of covid)-->
             <div class="dash-stats dash-test-item">
                 <GraphicTitle
-                        title={"Daily Positives for 9/12/2021"}
+                        title={"Daily Positives for 9/18/2021"}
                 />
                 <div class="donut-item dash-stats-item">
 
