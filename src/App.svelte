d23b72dd445c783eeba3e9cb5477bcbd7bc6faea
Xris Anderton <tikifez@users.noreply.github.com>
2021-09-27 09:30:46 -0400

    separated results by date into component

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
     import Meter_Chart from './charts/meter-chart.svelte';
     import Chart_Bar_Vertical from './charts/chart-bar-vertical.svelte'
     import Chart_Wellness_Summary from './charts/wellness-summary.svelte'
+    import Results_By_Date from './charts/results-by-date.svelte'
     import Data_Point from './charts/data-point.svelte'
     import SvelteTable from "svelte-table"
     import Waffle_Chart from "./charts/waffle-chart.svelte";
@@ -13,94 +14,10 @@
     import {negativePositive} from './helpers/colors.js'
     import Datepicker from 'svelte-calendar';
     import './global.css';
+    import jsonData from '../public/datasets/data.json';
 
     const meterColumnWidth = 66;
     const meterColumnLength = 406;
-
-    /**
-     * Gets a date object for today.
-     *
-     * @since 2.0
-     *
-     * @return Date
-     */
-    function getToday() {
-        return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
-    }
-
-    /**
-     * Gets a date object for a specified number of days ago.
-     *
-     * @since 2.0
-     *
-     * @return Date
-     */
-    function getDaysAgo(daysAgo) {
-        return new Date(getToday() - daysAgo * 24 * 60 * 60 * 1000);
-    }
-
-    /**
-     * Gets a date object for the start of the semester
-     *
-     * @since 2.0
-     *
-     * @return Date
-     */
-    function getSemesterStart() {
-        return new Date(filterEndDate - 7 * 24 * 60 * 60 * 1000);
-    }
-
-    /**
-     * Sets a single filter button as active.
-     */
-    function setActiveFilter(selector) {
-        let filterButtons = document.querySelectorAll('.button-filter');
-        let activeButton = document.querySelector(selector);
-        filterButtons.forEach((filterButton) => {
-            filterButton.classList.remove('is-active');
-        })
-
-        activeButton.classList.add('is-active');
-    }
-
-    /**
-     * Sets filter dates to past seven days
-     */
-    function setFilterLastSevenDays() {
-        filterStartDate = getDaysAgo(7);
-        filterEndDate = getToday();
-        setActiveFilter('.filter-seven-days');
-    }
-
-
-    /**
-     * Sets filter dates to past thirty days
-     */
-    function setFilterLastThirtyDays() {
-        filterStartDate = getDaysAgo(30);
-        filterEndDate = getToday();
-        setActiveFilter('.filter-thirty-days');
-    }
-
-    /**
-     * Sets filter dates to past thirty days
-     */
-    function setFilterThisSemester() {
-        filterStartDate = new Date("2021/09/02");
-        filterEndDate = new Date("2021/12/08");
-        setActiveFilter('.filter-semester');
-    }
-
-
-    /**
-     * Set up default dates
-     *
-     * @since 2.0
-     */
-    let filterEndDate = getToday();
-    let filterStartDate = getDaysAgo(7);
-    let isStartDateChosen = false;
-    let isEndDateChosen = false;
 
     /**
      * Get COVID dataset
@@ -112,6 +29,8 @@
 
     // the dollar sign is a Svelte-y way of declaring a variable that will be dynamic
     $: covidData = [];
+    // console.log(data);
+    // $: covidData = data;
 
 
     const parseTime = timeParse("%m/%d/%y");
@@ -138,18 +57,6 @@
         });
 
         covidData = data;
-    });
-
-    /**
-     * Filters test results by date.
-     *
-     * @since 2.0
-     */
-    $: filteredData = covidData.filter(function (d) {
-        const START_DATE = new Date(filterStartDate).getTime();
-        const END_DATE = new Date(filterEndDate).getTime();
-        const COMPARE_DATE = new Date(d.Date).getTime();
-        return (COMPARE_DATE >= START_DATE && COMPARE_DATE <= END_DATE);
     });
 
     // NEED THESE TO CYCLE THROUGH THE HEADERS OF THE GOOGLE SHEET
@@ -325,67 +232,10 @@
 
 
             <!-- Dashboard Filterable Test Results Stacked Bar Chart -->
-            <div class="chart-wrapper" id="chart-results-pos-neg">
-                <div class="dash-bars dash-test-item">
-                    <h2 class="section-heading">Test Results by Date</h2>
-                    <div class="filter-bar">
-                        <menu type="toolbar" class="filter-bar-presets">
-                            <button class="button-filter filter-seven-days is-active"
-                                    on:click={setFilterLastSevenDays}>Past 7
-                                Days
-                            </button>
-                            <button class="button-filter filter-thirty-days"
-                                    on:click={setFilterLastThirtyDays}>Past 30
-                                Days
-                            </button>
-                            <button class="button-filter filter-semester"
-                                    on:click={setFilterThisSemester}>Semester
-                            </button>
-                        </menu>
-
-                        <div class="filter-bar-date-range">
-                            <Datepicker bind:selected={filterStartDate}
-                                        bind:dateChosen={isStartDateChosen}
-                                        end={filterEndDate}>
-                                <div class="datepicker-label">{filterStartDate.toLocaleDateString()}</div>
-                            </Datepicker>
-                            <span class="date-separator"
-                                  aria-label="to">–</span>
-                            <Datepicker bind:selected={filterEndDate}
-                                        bind:dateChosen={isEndDateChosen}
-                                        start={filterStartDate}>
-                                <div class="datepicker-label">{filterEndDate.toLocaleDateString()}</div>
-                            </Datepicker>
-                        </div>
-                    </div>
-                    <div class="chart-results-pos-neg__chart">
-                        <div class="dashboard-legend">
-                            <div class="legendCells">
-                                <div class="cell1"></div>
-                                <div class="cell-label">NEGATIVE TESTS</div>
-
-                            </div>
-                            <div class="legendCells">
-                                <div class="cell2"></div>
-                                <div class="cell-label">POSITIVE TESTS</div>
-
-                            </div>
-
-                        </div>
-                        {#key filteredData}
-                            <Chart_Bar_Vertical
-                                    width={width_stacked}
-                                    height={height}
-                                    data={filteredData}
-                                    xVar={'Date'}
-                                    yVar={"Seven-Day Tests"}
-                                    yGroups={["Negative Tests", "Positive Tests"]}
-                                    colorscheme={negativePositive}
-                            />
-                        {/key}
-                    </div> <!-- /.chart-results-pos-neg__chart -->
-                </div>
-            </div> <!-- /.chart-results-pos-neg -->
+            <Results_By_Date
+                    data={covidData}
+                    title="Test Results by Date. ^_^"
+            ></Results_By_Date>
 
         </div>
 
---
+++ src/charts/results-by-date.svelte
@@ -0,0 +1,223 @@
+<script>
+    import {onMount} from 'svelte';
+    import {scaleOrdinal} from 'd3-scale';
+    import {mouse, select} from 'd3-selection';
+    import {entries} from 'd3-collection';
+    import {path} from 'd3-path';
+    import {arc, pie} from 'd3-shape';
+    import Chart_Bar_Vertical from './chart-bar-vertical.svelte'
+    import {negativePositive} from '../helpers/colors.js'
+    import Datepicker from 'svelte-calendar';
+
+
+    export let data = {data};
+    export let title = {title};
+    export let hasAccent = false;
+    // Dynamically figure out the width of CSS grid items.
+    let width = document.getElementById('covid-testing-dashboard').getBoundingClientRect().width;
+    let width_donut = Math.min(width, 175);
+    let width_stacked = 20;
+    let height = 500;
+    // height = Math.min(height, 500);
+
+    if (window.innerWidth > 600) {
+        width_donut = width * 0.22;
+        width_stacked = (width * 0.5)
+        height = width * 0.6;
+    }
+
+    let classNames = [
+        "graph",
+        "results-by-date"
+    ];
+
+    if (hasAccent) {
+        classNames.push("has-accent");
+    }
+
+    const getClassNames = () => {
+        return classNames.join(" ");
+    }
+
+
+    let d3 = {
+        select: select,
+    }
+    let el;
+
+
+    /**
+     * Generate the graph
+     */
+    onMount(generateGraph);
+
+    /**
+     * Gets a date object for today.
+     *
+     * @since 2.0
+     *
+     * @return Date
+     */
+    function getToday() {
+        return new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
+    }
+
+    /**
+     * Gets a date object for a specified number of days ago.
+     *
+     * @since 2.0
+     *
+     * @return Date
+     */
+    function getDaysAgo(daysAgo) {
+        return new Date(getToday() - daysAgo * 24 * 60 * 60 * 1000);
+    }
+
+    /**
+     * Gets a date object for the start of the semester
+     *
+     * @since 2.0
+     *
+     * @return Date
+     */
+    function getSemesterStart() {
+        return new Date(filterEndDate - 7 * 24 * 60 * 60 * 1000);
+    }
+
+    /**
+     * Sets a single filter button as active.
+     */
+    function setActiveFilter(selector) {
+        let filterButtons = document.querySelectorAll('.button-filter');
+        let activeButton = document.querySelector(selector);
+        filterButtons.forEach((filterButton) => {
+            filterButton.classList.remove('is-active');
+        })
+
+        activeButton.classList.add('is-active');
+    }
+
+    /**
+     * Sets filter dates to past seven days
+     */
+    function setFilterLastSevenDays() {
+        filterStartDate = getDaysAgo(7);
+        filterEndDate = getToday();
+        setActiveFilter('.filter-seven-days');
+    }
+
+
+    /**
+     * Sets filter dates to past thirty days
+     */
+    function setFilterLastThirtyDays() {
+        filterStartDate = getDaysAgo(30);
+        filterEndDate = getToday();
+        setActiveFilter('.filter-thirty-days');
+    }
+
+    /**
+     * Sets filter dates to past thirty days
+     */
+    function setFilterThisSemester() {
+        filterStartDate = new Date("2021/09/02");
+        filterEndDate = new Date("2021/12/08");
+        setActiveFilter('.filter-semester');
+    }
+
+    /**
+     * Set up default dates
+     *
+     * @since 2.0
+     */
+    let filterEndDate = getToday();
+    let filterStartDate = getDaysAgo(7);
+    let isStartDateChosen = false;
+    let isEndDateChosen = false;
+
+    /**
+     * Filters test results by date.
+     *
+     * @since 2.0
+     */
+    $: filteredData = data.filter(function (d) {
+        const START_DATE = new Date(filterStartDate).getTime();
+        const END_DATE = new Date(filterEndDate).getTime();
+        const COMPARE_DATE = new Date(d.Date).getTime();
+        return (COMPARE_DATE >= START_DATE && COMPARE_DATE <= END_DATE);
+    });
+
+
+    function generateGraph() {
+        const graphContainer = d3.select(el);
+
+        const graphVisualWrapper = graphContainer.append("div")
+            .attr("class", "graph-visual-wrapper");
+
+    }
+</script>
+
+<figure bind:this={el} class="{getClassNames()}">
+    <div class="chart-wrapper" id="chart-results-pos-neg">
+        <div class="dash-bars dash-test-item">
+            <h2 class="section-heading">{title}</h2>
+            <div class="filter-bar">
+                <menu type="toolbar" class="filter-bar-presets">
+                    <button class="button-filter filter-seven-days is-active"
+                            on:click={setFilterLastSevenDays}>Past 7
+                        Days
+                    </button>
+                    <button class="button-filter filter-thirty-days"
+                            on:click={setFilterLastThirtyDays}>Past 30
+                        Days
+                    </button>
+                    <button class="button-filter filter-semester"
+                            on:click={setFilterThisSemester}>Semester
+                    </button>
+                </menu>
+
+                <div class="filter-bar-date-range">
+                    <Datepicker bind:selected={filterStartDate}
+                                bind:dateChosen={isStartDateChosen}
+                                end={filterEndDate}>
+                        <div class="datepicker-label">{filterStartDate.toLocaleDateString()}</div>
+                    </Datepicker>
+                    <span class="date-separator"
+                          aria-label="to">–</span>
+                    <Datepicker bind:selected={filterEndDate}
+                                bind:dateChosen={isEndDateChosen}
+                                start={filterStartDate}>
+                        <div class="datepicker-label">{filterEndDate.toLocaleDateString()}</div>
+                    </Datepicker>
+                </div>
+            </div>
+            <div class="chart-results-pos-neg__chart">
+                <div class="dashboard-legend">
+                    <div class="legendCells">
+                        <div class="cell1"></div>
+                        <div class="cell-label">NEGATIVE TESTS</div>
+
+                    </div>
+                    <div class="legendCells">
+                        <div class="cell2"></div>
+                        <div class="cell-label">POSITIVE TESTS</div>
+
+                    </div>
+
+                </div>
+                {#key filteredData}
+                    <Chart_Bar_Vertical
+                            width={width_stacked}
+                            height={height}
+                            data={filteredData}
+                            title={"Test Results by Date"}
+                            xVar={'Date'}
+                            yVar={"Seven-Day Tests"}
+                            yGroups={["Negative Tests", "Positive Tests"]}
+                            colorscheme={negativePositive}
+                    />
+                {/key}
+            </div> <!-- /.chart-results-pos-neg__chart -->
+        </div>
+    </div> <!-- /.chart-results-pos-neg -->
+</figure>
